e33ace41292e164134fd5712fa06bcb6ecc2463e
ysfbzkrba <ba.yusufbozkir@gmail.com>
2020-11-18 13:46:16 +0300

    tabledigestor added

---
+++ SqlQueries/TableDigestor.sql
@@ -0,0 +1,24 @@
+/*
+			TABLE DIGESTOR
+*/
+USE Northwind
+
+/*
+SELECT 
+	CONCAT('@', c.name), ' ', t.name , '(', (C.max_length /2), ')'
+		FROM (sys.columns AS c join sys.types AS t ON c.user_type_id=t.user_type_id)  
+			WHERE c.object_id=OBJECT_ID('dbo.'+'@tablename');
+*/
+
+
+
+DECLARE @tablename nvarchar(20);
+
+DECLARE @query	nvarchar(max) =
+'SELECT 
+	CONCAT(''@'', c.name), '' '', t.name , ''('', (C.max_length /2), '')''
+		 FROM (sys.columns AS c join sys.types AS t ON c.user_type_id=t.user_type_id)  
+			 WHERE c.object_id=OBJECT_ID(''dbo.''+@tablename);'
+			 ;
+			 print @query
+
